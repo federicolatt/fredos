cf390eec2884c44808d320beae5b9fae7591cc3d
Federico Lattanzio <federicolattanzio31@gmail.com>
2026-06-17 22:42:13 +0200

    Final setup almost complete

--- Containerfile
+++ Containerfile
@@ -3,7 +3,11 @@ FROM scratch AS ctx
 COPY build_files /
 
 # Base Image
-FROM ghcr.io/rakuos/rakuos-base:latest
+#FROM ghcr.io/rakuos/rakuos-base:latest
+#FROM ghcr.io/rakuos/rakuos-base-nvidia:latest
+FROM ghcr.io/rakuos/rakuos-gnome:latest
+#FROM ghcr.io/rakuos/rakuos-gnome-nvidia:latest
+RUN sed -i 's/^ID=.*/ID=fedora/' /etc/os-release
 
 ## Other possible base images include:
 # FROM ghcr.io/ublue-os/bazzite:latest
@@ -28,6 +32,16 @@ FROM ghcr.io/rakuos/rakuos-base:latest
 ### MODIFICATIONS
 ## make modifications desired in your image and install packages by modifying the build.sh script
 ## the following RUN directive does all the things required to run "build.sh" as recommended.
+
+# Copy Homebrew files from the brew image
+# And enable
+COPY --from=ghcr.io/ublue-os/brew:latest /system_files /
+RUN --mount=type=cache,dst=/var/cache \
+    --mount=type=cache,dst=/var/log \
+    --mount=type=tmpfs,dst=/tmp \
+    /usr/bin/systemctl preset brew-setup.service && \
+    /usr/bin/systemctl preset brew-update.timer && \
+    /usr/bin/systemctl preset brew-upgrade.timer
 
 RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
     --mount=type=cache,dst=/var/cache \
--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,9 @@
 
 set -ouex pipefail
 
+## DNF5 Speedup
+sed -i '/^\[main\]/a max_parallel_downloads=10' /etc/dnf/dnf.conf
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -9,21 +12,80 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-
 ## System apps
 dnf -y install flatpak-builder # libvirt virt-manager qemu-kvm wlr-randr iotop sysstat lxqt-openssh-askpass lxpolkit parallel just seahorse
 
-# User apps
-dnf -y install nautilus gnome-terminal gnome-system-monitor gnome-calculator loupe
+### User apps
+dnf -y install nautilus gnome-terminal gnome-system-monitor gnome-calculator loupe kitty # bitwarden-cli lxpolkit
+# DEV packages
+# cargo evtest git input-remapper libevdev-devel libinput-utils python3-devel
 
-# Use a COPR Example:
+
+## Install Gnome extensions
+#https://github.com/dmy3k/auto-power-profile
 #
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
 
-#### Example for enabling a System Unit File
+## Install Nautilus extensions
+# nautilus-open-any-terminal
+#curl -Lo /etc/yum.repos.d/nautilus-open-any-terminal.repo \
+#  https://copr.fedorainfracloud.org/coprs/monkeygold/nautilus-open-any-terminal/repo/fedora-$(rpm -E %fedora)/monkeygold-nautilus-open-any-terminal-fedora-$(rpm -E %fedora).repo
+#dnf install -y nautilus-open-any-terminal
+#glib-compile-schemas /usr/share/glib-2.0/schemas
+#gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal kitty blackbox-terminal
 
+# install gnome-sushi (image viewer)
+
+
+# nautilus-admin
+
+
+# nautilus-copy-path
+
+# Install Niri 
+dnf -y install niri bibata-cursor-theme
+
+# Install Noctalia shell
+#curl -fsSL https://github.com/terrapkg/subatomic-repos/raw/main/terra.repo -o /etc/yum.repos.d/terra.repo
+#dnf -y install terra-release
+#dnf -y install noctalia-shell 
+# ABILITARE LE NOTIFICHE: systemctl --user enable --now swaync.service
+
+# Install Dank Linux shell
+sudo curl --output-dir "/etc/yum.repos.d/" \
+  --remote-name "https://copr.fedorainfracloud.org/coprs/avengemedia/dms/repo/fedora-$(rpm -E %fedora)/avengemedia-dms-fedora-$(rpm -E %fedora).repo"
+#
+# Install greetd login manager with dank configuration (still needs some work)
+mkdir -p /etc/greetd/
+cat > /etc/greetd/config.toml << EOF
+[terminal]
+vt = 1
+[default_session]
+user = "greeter"
+command = "dms-greeter --command niri"
+EOF
+rm -f /etc/systemd/system/display-manager.service
+ln -s /usr/lib/systemd/system/greetd.service /etc/systemd/system/display-manager.service
+systemctl enable --force greetd.service
+
+mkdir -p /etc/skel/.config/systemd/user/graphical-session.target.wants
+ln -s /usr/lib/systemd/user/dms.service /etc/skel/.config/systemd/user/graphical-session.target.wants/
+mkdir -p /etc/skel/.config/niri/
+cp -rf /ctx/dot_config/niri/config.kdl /etc/skel/.config/niri/
+
+
+
+#### Enable podman
 systemctl enable podman.socket
+
+
+# Remove waybar
+dnf -y remove waybar
+
+# this is needed for some glib applications
+glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+## CLEAN UP
+# Clean up dnf cache to reduce image size
+dnf5 -y clean all
+rm -rf /run/dnf /run/selinux-policy
+rm -rf /var/lib/dnf
